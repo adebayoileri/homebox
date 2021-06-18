fcca0861d2bb64408e18d191f547e41b93efa2ff
Adebayo Ilerioluwa <adebayorilerioluwa@gmail.com>
2021-06-18 14:57:34 +0100

    chore: add fetching data from API

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,59 @@
 import React from "react";
-import { HStack, Stack, Text, Box, Avatar, Flex } from "@chakra-ui/react";
+import {
+  HStack,
+  Stack,
+  Text,
+  Box,
+  Avatar,
+  Flex,
+  useToast,
+} from "@chakra-ui/react";
 import DataTable from "react-data-table-component";
+import axios from "axios";
 import DashboardLayout from "../../Layout/DashboardLayout";
 import StatsCard from "../../components/StatsCard";
-import { stats, customers } from "../../utils/data";
+import Loading from "../../components/Loading";
+import { stats } from "../../utils/data";
 import { customerColumns } from "../../utils/datatable";
 
 export default function Home() {
+  const toast = useToast();
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [customers, setCustomers] = React.useState([]);
+
+  React.useEffect(() => {
+    const getAllCustomers = async () => {
+      setIsLoading(true);
+      await axios
+        .get(`https://my.api.mockaroo.com/customers.json?key=4216a200`)
+        .then((res) => {
+          if (res.status === 200) {
+            setIsLoading(false);
+            setCustomers(res.data);
+          }
+        })
+        .catch((error) => {
+          setIsLoading(false);
+          if (error.message && error.message.indexOf("Network Error") !== -1) {
+            toast({
+              description:
+                "Network Error, Kindly check your internet connection and try again",
+              status: "error",
+            });
+          }
+
+          if (error.response && error.response.status === 500) {
+            toast({
+              description:
+                "Unable to reach our servers at this time, Kindly try again.",
+              status: "error",
+            });
+          }
+        });
+    };
+    getAllCustomers();
+  }, [toast]);
+
   return (
     <DashboardLayout>
       <Flex w="100%" justifyContent="space-between" alignItems="center">
@@ -37,6 +84,7 @@ export default function Home() {
         {stats &&
           stats.map((stat, idx) => (
             <StatsCard
+              key={idx}
               label={stat.label}
               numbers={stat.numbers}
               type={stat.type}
@@ -51,6 +99,8 @@ export default function Home() {
             columns={customerColumns}
             data={customers}
             pagination
+            progressPending={isLoading}
+            progressComponent={<Loading />}
             paginationPerPage={15}
           />
         </div>
